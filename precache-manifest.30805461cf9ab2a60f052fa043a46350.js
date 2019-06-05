a70d12007491ab8f31c0f208d74f81f032aea007
serkol <sergkol557@gmail.com>
2019-06-05 22:47:34 +0300

    Updates

---
+++ precache-manifest.30805461cf9ab2a60f052fa043a46350.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1168be2d30e2beea73bc639c321defe5",
+    "url": "/learn-mobx/index.html"
+  },
+  {
+    "revision": "666ee49916f4fedb2da4",
+    "url": "/learn-mobx/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8f16f4b3f9f8c0f38b2a",
+    "url": "/learn-mobx/static/js/2.6f0b1b61.chunk.js"
+  },
+  {
+    "revision": "666ee49916f4fedb2da4",
+    "url": "/learn-mobx/static/js/main.861b3415.chunk.js"
+  },
+  {
+    "revision": "56a4d8c9511294542936",
+    "url": "/learn-mobx/static/js/runtime~main.7c5b9db0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learn-mobx/static/media/logo.5d5d9eef.svg"
+  }
+]);
